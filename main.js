cb313057449bc5b4989a1eec33c015388db05717
kik4 <kik4.contact@gmail.com>
2021-11-07 15:13:04 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+javascript: (function () {
+  "use strict";
+
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  const downloadFromUrlAutomatically = (url, fileName) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "blob";
+    xhr.onload = function (e) {
+      if (this.status == 200) {
+        var urlUtil = window.URL || window.webkitURL;
+        var imgUrl = urlUtil.createObjectURL(this.response);
+        var link = document.createElement("a");
+        link.href = imgUrl;
+        link.download = fileName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    };
+    xhr.send();
+  };
+
+  const nodeList = document.querySelectorAll('.rtd>a[href^="/"]');
+  for (let i = 0; i < nodeList.length; i += 2) {
+    const href = nodeList[i].href;
+    const filename = href.split("/").slice(-1)[0];
+    downloadFromUrlAutomatically(href, filename);
+    sleep(1000);
+  }
+})();
